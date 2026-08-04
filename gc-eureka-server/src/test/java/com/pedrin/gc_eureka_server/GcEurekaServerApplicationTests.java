c70dcf76a79dd008f9f4bbfea3b596a94a9cc35b
PedrinL27 <pedrolangelineto0@gmail.com>
2026-08-04 12:10:34 -0300

    Added: Inicio do projeto com estrutura modular e configuracao inicial do eureka server

---
+++ gc-eureka-server/src/test/java/com/pedrin/gc_eureka_server/GcEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrin.gc_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
