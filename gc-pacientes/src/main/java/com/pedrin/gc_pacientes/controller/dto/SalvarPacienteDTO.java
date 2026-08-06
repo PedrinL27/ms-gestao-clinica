f904fbf689febf311c9f46bae8a77559ae019ebf
PedrinL27 <pedrolangelineto0@gmail.com>
2026-08-06 19:34:29 -0300

    Added: Conexao com banco de dados

--- gc-pacientes/src/main/java/com/pedrin/gc_pacientes/controller/dto/SalvarPacienteDTO.java
+++ gc-pacientes/src/main/java/com/pedrin/gc_pacientes/controller/dto/SalvarPacienteDTO.java
@@ -1,12 +1,27 @@
 package com.pedrin.gc_pacientes.controller.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.br.CPF;
+
 import java.time.LocalDate;
 
 public record SalvarPacienteDTO(
+        @NotBlank(message = "Campo requerido")
+        @Size(min = 2, max = 70)
         String nome,
+        @CPF
+        @NotBlank(message = "Campo requerido")
         String cpf,
+        @NotBlank(message = "Campo requerido")
+        @Size(min = 5, max = 20)
         String telefone,
+        @NotBlank(message = "Campo requerido")
+        @Size(min = 5, max = 50)
         String email,
+        @NotBlank(message = "Campo requerido")
+        @Past(message = "Nao pode ser uma data futura")
         LocalDate dataNascimento
 ) {
 }
